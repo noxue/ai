f64c7799212916168f224ef2daa43bbd87b743fb
刘荣飞 <yes@noxue.com>
2018-06-04 16:21:26 +0800

    修改generatorConfig.xml配置，生成mysql语句 关键字 加反引号。修改了部分数据库字段。

--- src/main/java/com/ai/dao/SimUserMapper.java
+++ src/main/java/com/ai/dao/SimUserMapper.java
@@ -3,7 +3,15 @@ package com.ai.dao;
 import com.ai.domain.bo.SimUser;
 
 public interface SimUserMapper {
+    int deleteByPrimaryKey(Long id);
+
     int insert(SimUser record);
 
     int insertSelective(SimUser record);
+
+    SimUser selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(SimUser record);
+
+    int updateByPrimaryKey(SimUser record);
 }
