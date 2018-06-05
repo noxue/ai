065de101cf30920cbdbe4587f3a48cec51bde1d8
刘荣飞 <yes@noxue.com>
2018-06-05 11:06:43 +0800

    x

--- src/main/java/com/ai/service/impl/VoiceServiceImpl.java
+++ src/main/java/com/ai/service/impl/VoiceServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Configuration
@@ -34,17 +36,26 @@ public class VoiceServiceImpl implements VoiceService {
     @Override
     public boolean upload(MultipartFile voice) throws IOException,IllegalStateException {
 
-        String fileName = voice.getOriginalFilename();
-        int size = (int) voice.getSize();
-        System.out.println(fileName + "-->" + size);
+        // 这里只是简单例子，文件直接输出到项目路径下。
+        // 实际项目中，文件需要输出到指定位置，需要在增加代码处理。
+        // 还有关于文件格式限制、文件大小限制，详见：中配置。
+        BufferedOutputStream out = new BufferedOutputStream(
+                new FileOutputStream(new File(voice.getOriginalFilename())));
+        out.write(voice.getBytes());
+        out.flush();
+        out.close();
 
-        String path = "e:/test" ;
-        File dest = new File(path + "/" + fileName);
-        if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
-            dest.getParentFile().mkdir();
-        }
-
-        voice.transferTo(dest); //保存文件
+//        String fileName = voice.getOriginalFilename();
+//        int size = (int) voice.getSize();
+//        System.out.println(fileName + "-->" + size);
+//
+//        String path = "static/upload" ;
+//        File dest = new File(path + "/" + fileName);
+//        if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
+//            dest.getParentFile().mkdir();
+//        }
+//
+//        voice.transferTo(dest); //保存文件
         return true;
     }
 }
