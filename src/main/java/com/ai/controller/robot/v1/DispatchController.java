82168db2250467bbcaad01225a130da2ac4c67a3
刘荣飞 <yes@noxue.com>
2018-09-08 15:59:03 +0800

    x

---
+++ src/main/java/com/ai/controller/robot/v1/DispatchController.java
@@ -0,0 +1,2 @@
+package com.ai.controller.robot.v1;
+
---
+++ src/main/java/com/ai/controller/robot/v1/WebsocketConfig.java
@@ -0,0 +1,8 @@
+package com.ai.controller.robot.v1;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSocket
+public class WebsocketConfig {
+}
